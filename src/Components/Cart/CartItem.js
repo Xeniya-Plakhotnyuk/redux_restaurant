2c2a54295ae4620af896da52556b0763b58b6cba
XeniyaZ <kseniya10081989@gmail.com>
2023-05-18 15:04:54 -0500

    create cart calculation

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,9 +1,14 @@
+import dishesData from "../../data/DishesData";
 
-const CartItem = ({cartItem}) =>{
-    return(
-        <div>
-          <p>{cartItem.quantity} portion(s)</p>  
-        </div>
-    )
-}
+const CartItem = ({ cartItem }) => {
+  const dishes = dishesData.find((item) => item.id === cartItem.dishId);
+
+  return (
+    <div>
+      <p>{dishes.name}</p>
+      <p>{cartItem.quantity} portion(s)</p>
+      <p>Price: ${dishes.price * cartItem.quantity}</p>
+    </div>
+  );
+};
 export default CartItem;
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -11,12 +11,19 @@ export  const  cartSlice = createSlice({
         addItemToCart: (state, action) =>{ 
             state.cartItems.push({
                 dishId: action.payload.dish.id,
-                quantity: action.payload.quantity
+                quantity: action.payload.quantity,
+                totalPrice: action.payload.quantity * action.payload.dish.price
             })
         }
     }
 
 })
+
+export const getTotalPrice = state => {
+    return state.cart.cartItems.reduce((total, cartItems) =>{
+        return cartItems.totalPrice + total
+    }, 0)
+}
 
 export const getCartItems = state => state.cart.cartItems;
 export const  { addItemToCart } = cartSlice.actions;
